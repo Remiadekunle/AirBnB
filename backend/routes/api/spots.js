833efdc103ab32bf5be2f2f2094e994448cb750c
Remiadekunle <remiadekunle21@gmail.com>
2022-11-22 10:00:14 -0800

    get-all-spots finished

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,25 +1,50 @@
 const express = require('express')
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { Spot, User, SpotImage } = require('../../db/models');
+const { Spot, User, SpotImage, Review, sequelize } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll({
-        include: {
-            model: SpotImage,
-            where: {
-                preview: true
+        include: [
+            {
+                model: SpotImage,
+                where: {
+                    preview: true
+                },
+                attributes: {
+                    exclude: ['id', 'spotId', 'createdAt', 'updatedAt']
+                }
             },
-            // as: 'previewImage',
-            attributes: {
-                exclude: ['id', 'spotId', 'createdAt', 'updatedAt', 'preview']
+            {
+                model: Review,
             }
-        }
+        ]
     })
+    const Spots = [];
+    spots.forEach(spot => {
+        const newSpot = spot.toJSON();
+        Spots.push(newSpot)
+    })
+    Spots.forEach((spot) => {
+        spot.SpotImages.forEach( (image) => {
+            if (image.preview === true){
+                spot.previewImage = image.url
+            }
+        })
+        delete spot.SpotImages
+        let scores = 0
+        spot.Reviews.forEach(review => {
+            scores += review.stars
+        })
+        const stars = scores/spot.Reviews.length
+        spot.avgRating = stars
+        delete spot.Reviews
+    })
+
     return res.json({
-        spots
+        Spots
     })
 })
 
