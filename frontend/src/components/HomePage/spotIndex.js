02c1df02a25cf27ddf14143d2c7057a8fe35bae3
Remiadekunle <remiadekunle21@gmail.com>
2022-12-12 13:53:16 -0800

    fixing up the spot cards

--- frontend/src/components/HomePage/spotIndex.js
+++ frontend/src/components/HomePage/spotIndex.js
@@ -1,10 +1,29 @@
 import './homePage.css';
 
 export function SpotIndex( {spot} ){
+    const { city, state, price, name, avgRating} = spot
+
+    // if (typeof avgRating !== 'number'){
+    //     avgRating = 'NEW'
+    // }
+
     return (
         <div className="home-Card">
-            {spot.id}
-            <img alt='' className='preview-img'  src={`${spot.previewImage}` }></img>
+            <div className='img-border' >
+                <img alt='' className='preview-img'  src={`${spot.previewImage}` }></img>
+            </div>
+            <div className='spot-contents'>
+                <div className='rating-location'>
+                    <span> {`${city}, ${state}`}</span>
+                    <div className='rating'>{avgRating}</div>
+                </div>
+                <span className='card-info'>
+                {name}
+                </span>
+                <span>
+                {`$${price} night`}
+                </span>
+            </div>
         </div>
     )
 }
