ee6585fdf0b6c70c5c8fbc3056e7f29640c9589a
Remiadekunle <remiadekunle21@gmail.com>
2023-02-20 17:43:06 -0800

    for render

--- backend/db/seeders/20221121234312-demo-reviews.js
+++ backend/db/seeders/20221121234312-demo-reviews.js
@@ -148,7 +148,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1,2,3,4,5] }
+      spotId: { [Op.in]: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] }
     }, {})
   }
 };
--- backend/db/seeders/20221121235802-demo-spotimages.js
+++ backend/db/seeders/20221121235802-demo-spotimages.js
@@ -116,7 +116,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1,2,3,4,5] }
+      spotId: { [Op.in]: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] }
     }, {})
   }
 };
